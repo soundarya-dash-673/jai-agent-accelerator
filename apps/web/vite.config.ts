86d0b073354cf8704d2243a3983a525b64ad03ff
Soundarya Dash <dashsoundarya@gmail.com>
2025-12-29 18:47:17 -0500

    proxy updatses

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      port: 3000,
+      // port: 3000,
       proxy: {
         '/api': {
           // Use env var, fallback to localhost
